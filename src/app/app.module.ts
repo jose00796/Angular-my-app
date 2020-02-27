42b3cf18d79a18854f398f8cf0d3f07929c30f21
jose00796 <josebalboa11@hotmail.com>
2020-02-27 10:13:08 -0400

    Agregamos un componente de prueba llamado contenido y le pusimos nuestro horario de esclavitud

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,12 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { ContenidoComponent } from './contenido/contenido.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ContenidoComponent
   ],
   imports: [
     BrowserModule
